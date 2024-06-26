bbd46b759b16cf792eb8a5ba502b344d7ee9dddd
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-06-26 16:19:44 -0400

    Change how configureBlockDisplay works

--- src/main/java/com/drgnfireyellow/kite/Display.java
+++ src/main/java/com/drgnfireyellow/kite/Display.java
@@ -1,19 +1,18 @@
 package com.drgnfireyellow.kite;
 
-import org.bukkit.Location;
 import org.bukkit.entity.BlockDisplay;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
 import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
 
 public class Display {
-    public static void configureBlockDisplay(@NotNull BlockDisplay display, @NotNull Material type, double translationX, double translationY, double translationZ, double scaleX, double scaleY, double scaleZ, float rotationX, float rotationY, float rotationZ) {
+    public static BlockDisplay configureBlockDisplay(@NotNull BlockDisplay display, @NotNull Material type, double translationX, double translationY, double translationZ, double scaleX, double scaleY, double scaleZ, float rotationX, float rotationY, float rotationZ) {
         display.setBlock(type.createBlockData());
         Transformation transformation = display.getTransformation();
         transformation.getTranslation().set(translationX, translationY, translationZ);
         transformation.getScale().set(scaleX, scaleY, scaleZ);
         transformation.getRightRotation().set(rotationX, rotationY, rotationZ, 1);
         display.setTransformation(transformation);
+        return display;
     }
 }
