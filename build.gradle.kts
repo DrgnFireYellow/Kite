a1d36dd45f35bf4c37e1f8f2a7fb4d24c2f49264
DrgnFireYellow <86269995+DrgnFireYellow@users.noreply.github.com>
2024-06-26 13:29:50 +0000

    I'm getting really tired of troubleshooting jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,3 +16,14 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     implementation("com.github.DrgnFireYellow:kite:master-SNAPSHOT")
 }
+
+publishing {
+    publications {
+        // Creates a Maven publication called "release".
+        register("release", MavenPublication::class) {
+            groupId = "com.drgnfireyellow"
+            artifactId = "kite"
+            version = "1.0.0"
+        }
+    }
+}
