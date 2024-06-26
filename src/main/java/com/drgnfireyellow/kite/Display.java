e6207c16a8c3a8af98ffc0a34272cb9b54fbe832
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-06-26 16:02:45 -0400

    Change to be manipulateBlockDisplay

--- src/main/java/com/drgnfireyellow/kite/Display.java
+++ src/main/java/com/drgnfireyellow/kite/Display.java
@@ -8,14 +8,12 @@ import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
 
 public class Display {
-    public static BlockDisplay createBlockDisplay(@NotNull Material type, @NotNull Location position, double translationX, double translationY, double translationZ, double scaleX, double scaleY, double scaleZ, float rotationX, float rotationY, float rotationZ) {
-        BlockDisplay display = (BlockDisplay) position.getWorld().spawnEntity(position, EntityType.BLOCK_DISPLAY);
+    public static void configureBlockDisplay(@NotNull BlockDisplay display, @NotNull Material type, double translationX, double translationY, double translationZ, double scaleX, double scaleY, double scaleZ, float rotationX, float rotationY, float rotationZ) {
         display.setBlock(type.createBlockData());
         Transformation transformation = display.getTransformation();
         transformation.getTranslation().set(translationX, translationY, translationZ);
         transformation.getScale().set(scaleX, scaleY, scaleZ);
-//        transformation.getRightRotation().set(rotationX, rotationY, rotationZ, 1);
+        transformation.getRightRotation().set(rotationX, rotationY, rotationZ, 1);
         display.setTransformation(transformation);
-        return display;
     }
 }
