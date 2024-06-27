5b15c913b1c07d6d182929defc506b5e556b74f2
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-06-27 08:22:14 -0400

    Change parameters

--- src/main/java/com/drgnfireyellow/kite/Display.java
+++ src/main/java/com/drgnfireyellow/kite/Display.java
@@ -6,15 +6,16 @@ import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Vector3f;
 
 public class Display {
-    public static BlockDisplay createBlockDisplay(@NotNull Material type, @NotNull Location position, double translationX, double translationY, double translationZ, double scaleX, double scaleY, double scaleZ, float rotationX, float rotationY, float rotationZ) {
+    public static BlockDisplay createBlockDisplay(@NotNull Material type, @NotNull Location position, Vector3f translation, Vector3f scale, Vector3f rotation) {
         BlockDisplay display = (BlockDisplay) position.getWorld().spawnEntity(position, EntityType.BLOCK_DISPLAY);
         display.setBlock(type.createBlockData());
         Transformation transformation = display.getTransformation();
-        transformation.getTranslation().set(translationX, translationY, translationZ);
-        transformation.getScale().set(scaleX, scaleY, scaleZ);
-        transformation.getRightRotation().set(rotationX, rotationY, rotationZ, 1);
+        transformation.getTranslation().set(translation);
+        transformation.getScale().set(scale);
+        transformation.getRightRotation().set(rotation.x, rotation.y, rotation.z, 1);
         display.setTransformation(transformation);
         return display;
     }
